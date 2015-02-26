3d869c981bf20fa57015a01b5ec31852fc948eb4
Shubham Jain <shubhja@cse.iitk.ac.in>
2015-02-26 15:36:05 +0530

    error in hour removed as it previously incremented it throughout the minute 59

--- assignment4/clock.sh
+++ assignment4/clock.sh
@@ -1,12 +1,14 @@
 sec=50
-min=03
+min=58
 hour=15
 while [ 1 ]
 do
     clear
-	hour=$((($hour + $min/59)%24))
+	if [ "$min" -eq 59 ] && [ "$sec" -eq 59 ] ; then
+		hour=$((($hour + 1)%24))
+	fi
 	min=$((($min + $sec/59)%60))
 	sec=$((($sec+1)%60))
 	banner "$hour:$min:$sec"
-    sleep 1
+    sleep 0.1
 done
