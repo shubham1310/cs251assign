0d6b210f741bdca55df8d23ff6d49948b2e3aeec
Shubham Jain <shubhja@cse.iitk.ac.in>
2015-02-28 16:05:44 +0530

    clock showing the time on the computer added with the previous code commented out

--- assignment4/clock.sh
+++ assignment4/clock.sh
@@ -1,14 +1,22 @@
-sec=50
-min=58
-hour=15
 while [ 1 ]
 do
-    clear
-	if [ "$min" -eq 59 ] && [ "$sec" -eq 59 ] ; then
-		hour=$((($hour + 1)%24))
-	fi
-	min=$((($min + $sec/59)%60))
-	sec=$((($sec+1)%60))
-	banner "$hour:$min:$sec"
-    sleep 0.1
+	clear
+	i="$(date +%T)"
+	banner $i
+	sleep 1
 done
+# In case the clock is to be made from scratch use this code
+# sec=50
+# min=58
+# hour=15
+# while [ 1 ]
+# do
+#     clear
+# 	if [ "$min" -eq 59 ] && [ "$sec" -eq 59 ] ; then
+# 		hour=$((($hour + 1)%24))
+# 	fi
+# 	min=$((($min + $sec/59)%60))
+# 	sec=$((($sec+1)%60))
+# 	banner "$hour:$min:$sec"
+#     sleep 0.1
+# done
