daf09cef5e5448b0eb75931d1c7215a44ba17348
Shubham Jain <shubhja@cse.iitk.ac.in>
2015-03-26 16:22:06 +0530

    gaussian dist implemented

---
+++ assignment5/gau/gauss.m
@@ -0,0 +1,8 @@
+y=normrnd(15,sqrt(10.5),1000,1);
+save gauss y
+y=normrnd(15,sqrt(10.5),10000,1);
+save gauss1 y
+y=normrnd(15,sqrt(10.5),100000,1);
+save gauss2 y
+y=normrnd(15,sqrt(10.5),1000000,1);
+save gauss3 y
